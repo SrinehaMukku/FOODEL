2ee389abcffa4d3c24628723ceab8660eca7e5a9
Srineha_mukku <mukkusrineha.com>
2025-02-10 18:34:01 +0530

    frontend designed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React,{useState} from 'react';
+import Navbar from './components/navbar/navbar';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Footer from './components/Footer/Footer';
+import LoginPopup from './components/LoginPopup/LoginPopup';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+const App = () => {
+
+  const [showLogin,setShowLogin]=useState(false)
+  return (
+    <>
+    {showLogin?<LoginPopup/>:<></>}
+    <div className='app'> 
+      <Navbar setShowLogin={setShowLogin}/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/place" element={<PlaceOrder />} />
+      </Routes>
+    </div>
+    <Footer/>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-contents'>
+        <h2>Order your fav</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe laudantium ipsam necessitatibus, quas voluptatibus odio consequatur nemo, laboriosam iure soluta qui, quaerat at nulla? Laborum est sunt beatae quasi reprehenderit.</p>
+        <button>View Menu</button>
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
