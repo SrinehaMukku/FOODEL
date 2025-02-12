df928668cc87541afc74850f9aff27636c8f4640
Srineha_mukku <mukkusrineha.com>
2025-02-12 18:43:43 +0530

    backend finalised

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -23,13 +23,26 @@ const addFood = async (req, res) => {
 }
 //all food list
 const listFood = async (req, res) => {
-     try{
+    try {
         const foods = await foodModel.find({});
-        res.json({success:true,data:foods})
-     }catch(error){
+        res.json({ success: true, data: foods })
+    } catch (error) {
         console.log("error")
-        res.json({success:true,data:foods})
-     }
+        res.json({ success: false, data: foods })
+    }
 }
 
-export { addFood, listFood };
+//remove food
+const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`, () => { })
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({ success: true, message:"Food Removed"})
+    } catch (error) {
+        console.log("error")
+        res.json({ success: false, message:"Error"})
+    }
+}
+
+export { addFood, listFood, removeFood };
